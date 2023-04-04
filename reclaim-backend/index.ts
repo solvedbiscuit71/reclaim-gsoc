f7481f791eb36f90edbc5686e3948dd53bc8d680
Praveen Perumal <ppraveen98841@gmail.com>
2023-04-04 06:49:56 +0530

    added /organisers route and cors()

--- reclaim-backend/index.ts
+++ reclaim-backend/index.ts
@@ -4,17 +4,35 @@ import dotenv from "dotenv";
 dotenv.config()
 
 const app: Application = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
+
+import cors from 'cors';
+const corsOptions = {
+    origin: process.env.FRONTEND_BASE_URL || 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    optionsSuccessStatus: 200
+}
 
 // Body parsing Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions))
 
 app.get(
-    "/",
+    "/organisers",
     async (req: Request, res: Response): Promise<Response> => {
         return res.status(200).send({
-            message: "Welcome to reclaim protocol",
+            organisers: [
+                ["AsyncAPI",  "asyncapi/spec"],
+                ["Conda", "conda/conda"],
+                ["FluxCD", "fluxcd/flux2"],
+                ["OQ Engine", "gem/oq-engine"],
+                ["Matplotlib", "matplotlib/matplotlib"],
+                ["MicroPython", "micropython/micropython"],
+                ["Numpy", "numpy/numpy"],
+                ["Wagtail", "wagtail/wagtail"],
+                ["WasmEdge", "WasmEdge/WasmEdge"],
+            ]
         });
     }
 );
