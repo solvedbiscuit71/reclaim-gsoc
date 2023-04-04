7b4e6c381931806ccdd3a1bce4e41a98ea8c4f3f
Praveen Perumal <ppraveen98841@gmail.com>
2023-04-04 09:19:14 +0530

    added /verify API

--- reclaim-backend/index.ts
+++ reclaim-backend/index.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response } from "express";
+import { Reclaim, generateUuid } from "@reclaimprotocol/reclaim-sdk";
+
 import dotenv from "dotenv";
 
 dotenv.config()
 
 const app: Application = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 import cors from 'cors';
 const corsOptions = {
@@ -17,6 +19,37 @@ const corsOptions = {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions))
+
+// Reclaim protocol
+const callbackUrl = process.env.CALLBACK_URL || 'http://localhost:3000'
+const reclaim = new Reclaim(callbackUrl + '/callback/')
+
+app.post(
+    "/verify",
+    async (req: Request, res: Response): Promise<Response> => {
+        const repoName = req.body.repo
+        const callbackId = 'repo-' + generateUuid()
+
+        const template = (
+            await reclaim.connect('Github-contributor', [
+                {
+                    provider: 'github-contributor',
+                    params: {
+                        repo: repoName
+                    }
+                }
+            ])
+        ).generateTemplate(callbackId)
+
+        const url = template.url
+        const templateId = template.id
+
+        return res.status(200).send({
+            url,
+            callbackId
+        })
+    }
+)
 
 app.get(
     "/organisers",
